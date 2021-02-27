22f43daad23feff8f624df90c2962f028c6e380e
Yashwanth <t.yashwanthreddy@gmail.com>
2021-02-27 11:45:08 -0600

    Initial commit

---
+++ src/test/java/com/oAuth2example/oAuth2/OAuth2MicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oAuth2example.oAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2MicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
